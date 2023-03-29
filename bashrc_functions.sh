5748ac15246326f802c7fade9de3a5f6a5d20873
Shyam Yadati <shyam.yadati@gmail.com>
2023-03-29 17:30:47 -0400

    renamed bash functions

--- bashrc_functions.sh
+++ bashrc_functions.sh
@@ -1,33 +1,30 @@
 
-function sim-run()
+function sim.run()
 {
     docker run -itd \
         --name=dev-sim \
         -h dev-sim-container \
         -v $HOME:/home/$USER \
+        -v /data:/data \
         -v /var/docker.sock:/var/run/docker.sock \
         -v /etc/passwd:/etc/passwd:ro \
         -v /etc/group:/etc/group:ro \
         --user=$(id -u $USER):$(id -g $USER) \
         -e DISPLAY=$DISPLAY \
         --network host \
-        sandbox-eda/dev-sim-container:latest
+        slyad/dev-sim:latest
 }
 
-
-       # -e USER=$USER \
-       # -e USERID=$(id -u) \
-
-function sim-start() {
+function sim.restart() {
     docker restart dev-sim
 }
 
-function sim-enter()
+function sim.enter()
 {
     docker exec -it -u $(id -u $USER):$(id -g $USER) dev-sim bash
 }
 
-function sim-stop()
+function sim.stop()
 {
     docker stop dev-sim
     docker rm dev-sim
